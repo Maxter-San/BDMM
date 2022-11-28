77340e9afabab51978236288f2369f4504f1a973
Maxter-San <102878819+Maxter-San@users.noreply.github.com>
2022-11-28 17:08:45 -0600

    WEBGL

---
+++ WEBGL/conexion.php
@@ -0,0 +1,57 @@
+<?php
+
+	$action = $_POST['action'];
+
+	if ($action == "addPuntaje") 
+		addPuntaje();
+	else if ($action == "getPuntaje")
+    getPuntaje();
+
+	function connect() {
+		$databasehost = "localhost:3306";
+		$databasename = "webgl";
+		$databaseuser = "root";
+		$databasepass = "35652515";
+
+		$mysqli = new mysqli($databasehost, $databaseuser, $databasepass, $databasename);
+		if ($mysqli->connect_errno) {
+			echo "Problema con la conexion a la base de datos";
+		}
+		return $mysqli;
+	}
+
+
+
+	function addPuntaje() {
+		$name = $_POST["name"];
+		$puntaje = $_POST["puntaje"];
+		$nivel = $_POST["nivel"];
+		$mysqli = connect();
+
+		$result = $mysqli->query("INSERT INTO puntaje(nombre, puntaje, nivel) values('".$name."','".$puntaje."','".$nivel."')");	
+		
+		if (!$result) {
+			echo "Problema al hacer un query: " . $mysqli->error;								
+		} else {
+			echo "Todo salio bien";		
+		}
+		mysqli_close($mysqli);
+	}
+
+	function getPuntaje() {
+		$mysqli = connect();
+
+		$result = $mysqli->query("Select * from puntaje order by puntaje asc limit 10");	
+		
+		if (!$result) {
+			echo "Problema al hacer un query: " . $mysqli->error;								
+		} else {
+			$rows = array();
+			while( $r = $result->fetch_assoc()) {
+				$rows[] = $r;
+			}			
+			echo json_encode($rows);
+		}
+		mysqli_close($mysqli);
+	}
+?>

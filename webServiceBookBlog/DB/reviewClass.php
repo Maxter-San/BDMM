8951210bd92375ea55a03d34e31534718dca58a9
Maxter-San <102878819+Maxter-San@users.noreply.github.com>
2022-12-02 01:42:41 -0600

    bookblog

--- webServiceBookBlog/DB/reviewClass.php
+++ webServiceBookBlog/DB/reviewClass.php
@@ -79,7 +79,7 @@
         }
 
         function getAllReviewsDetail(){
-            $query = $this->connect()->query('SELECT reviewId, T1.creationDate, status, title, description, image, valoration, book, T1.userId, name, profilePhoto
+            $query = $this->connect()->query('SELECT reviewId, T1.creationDate, status, title, description, image, feedback, book, T1.userId, name, profilePhoto
                                               FROM review AS T1
                                               INNER JOIN user AS T2
                                               ON T1.userId = T2.userId

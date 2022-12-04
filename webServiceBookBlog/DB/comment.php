ca79c0e1ca52eda1edbf7962a6aba437c5b4a678
Maxter-San <102878819+Maxter-San@users.noreply.github.com>
2022-12-03 19:37:00 -0600

    comments

---
+++ webServiceBookBlog/DB/comment.php
@@ -0,0 +1,42 @@
+<?php 
+    include_once 'conection.php';
+
+    class commentClass extends DB{
+        /*
+        userId
+        creationDate
+        
+        name
+        mail
+        password
+        cellphone
+        address
+        profilePhoto
+        
+        userType
+        */
+
+        function insertComment(
+            $comment,
+            $reviewId
+        ){
+            $query = $this->connect()->query('
+                                            INSERT INTO comment (comment, reviewId)
+                                            VALUES("'.$comment.'", '.$reviewId.');
+                                            ');
+            //$this->connect()->close();
+            return $query;
+        }
+
+
+        function getCommentByReview($reviewId)
+        {
+            $query = $this->connect()->query('SELECT * FROM comment WHERE reviewId = '.$reviewId.';');
+            //$this->connect()->close();
+            return $query;
+        }
+
+
+    }
+
+?>
---
+++ webServiceBookBlog/comments.php
@@ -0,0 +1,53 @@
+<?php 
+    include_once('DB/comment.php');
+    $commentClass = new commentClass();
+
+    if($_SERVER['REQUEST_METHOD'] == 'GET'){
+        header("HTTP/1.1 200 ok");
+
+        if(isset($_GET['reviewId'])){
+            $res = $commentClass->getCommentByReview($_GET['reviewId']);
+            $rows= [];
+            while($r = mysqli_fetch_assoc($res)) {
+                $rows[] = $r;
+            }
+            echo json_encode($rows);
+        }
+
+        exit;
+    }
+
+    else if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        
+        
+            header("HTTP/1.1 200 okee");
+
+            //$HTTP_RAW_POST_DATA
+            $json = file_get_contents('php://input');
+
+            // Converts it into a PHP object
+            $data = json_decode($json, true);
+
+            //echo $data['userId'];
+            //exit;
+        
+        if(isset($data['comment']) && isset($data['reviewId'])){
+            header("HTTP/1.1 200 ok");
+
+
+            $res = $commentClass->insertComment($data['comment'],
+                                              $data['reviewId']
+            );
+            echo $json;
+            exit;
+
+        }else{
+            header("HTTP/1.1 400 faltan datos");
+        }
+
+        exit;
+    }
+
+    
+
+?>

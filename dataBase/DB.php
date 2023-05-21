1312ffda0468bf5e22a93157d770b9ed9ee04287
Maxter-San <102878819+Maxter-San@users.noreply.github.com>
2023-05-21 01:41:31 -0600

    root

--- dataBase/DB.php
+++ dataBase/DB.php
@@ -11,7 +11,7 @@
             //$this->password = "Pass741852963";
             //$this->servername = "159.223.191.152";
             $this->servername = "198.199.88.16";
-            $this->username = "shop";
+            $this->username = "root";
             $this->password = "password";
             $this->dbname = "MAXTERSHOP";
         }
